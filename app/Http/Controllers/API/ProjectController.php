44494dea62b237847a7c7dde9cd289cc22953be9
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-04-15 16:53:51 +0200

    feat: add show api

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -15,9 +15,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::select('id', 'type_id' ,'title', 'github_reference', 'description', 'image')->with(['type:label,color', 'technologies:label,color'])->paginate();
+        $projects = Project::select('id', 'type_id' ,'title', 'github_reference', 'description', 'image')->with(['type:id,label,color', 'technologies:id,label,color'])->paginate();
         foreach ($projects as $project){
-            $project->image = asset('storage/'. $project->image);
+            $project->image = asset("/storage/". $project->image) ?? 'https://placehold.co/600x400';
             $project->description = $project->get_description(60);
         }
         return response()->json($projects);
@@ -42,7 +42,8 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        //
+        $project = Project::where('id', $id)->with(['type:id,label,color', 'technologies:id,label,color'])->first();
+        return response()->json($project)
     }
 
     /**
